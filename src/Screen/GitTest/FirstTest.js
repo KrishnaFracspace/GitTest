e6c324f566bbecfe990fd17f3235d92f58a7ea32
Krishna <krishna@fracspace.com>
2025-12-01 09:52:23 +0530

    added new test

--- src/Screen/GitTest/FirstTest.js
+++ src/Screen/GitTest/FirstTest.js
@@ -5,6 +5,7 @@ export default function FirstTest() {
   return (
     <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
       <Text style={{fontFamily:'Montserrat-Bold',fontSize:13,color:'#000'}}>Hello WOrld from First Git</Text>
+      <Text>Added new line....</Text>
     </View>
   )
 }
